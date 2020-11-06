2bba0319465f5564e951c29ae99d9eb0a21ea0fb
p0isonBR <65384918+p0isonBR@users.noreply.github.com>
2020-11-06 01:31:37 -0300

    Update consultaCPF.py

--- consultaCPF.py
+++ consultaCPF.py
@@ -60,7 +60,7 @@ Consulta de CPF gratis!''')
 def consulta():
   cpf=input(f'{C}[{G}+{C}]Informe o CPF a ser consultado (sem pontos ou traços): {B}')
  
-  result=requests.requests('GET', a+cpf+b).json()
+  result=requests.request('GET', a+cpf+b).json()
   if result['status']=='false':
     print('{C}[{R}-{C}]Numero nao encontrado na base da Receita Federal, tente outro.')
     consulta()
